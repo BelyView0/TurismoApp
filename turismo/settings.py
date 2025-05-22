de22cdceb1a328924426a49c124524a40fd6d41e
Monsse Marin <monsse.mv19@gmail.com>
2025-05-22 09:58:15 -0600

    se agrego la bd y superuser

--- turismo/settings.py
+++ turismo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'turismo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dbturismo.sqlite3',
     }
 }
 
